355ab87ccc13516716f6cb39b110a68693f72cf3
legendemil <emilpausz@gmail.com>
2017-06-16 12:02:18 +0200

    add /projects route, add projects.hbs view

--- server.js
+++ server.js
@@ -57,6 +57,13 @@ app.get('/bad', (req, res) => {
 	});
 });
 
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		pageTitle: 'My awesome projects',
+		welcomeMsg: 'My awesome projects'
+	});
+});
+
 app.listen(port, () => {
 	console.log('Server is up on port ${port}');
 });
